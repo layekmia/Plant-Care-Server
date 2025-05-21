01084aadbe111217f1d3bb1f6bd29fa2025eb228
Layek Miah <layekofficial63@gmail.com>
2025-05-21 16:17:49 +0600

    make code more clean and effecent

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
 const expres = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 const router = require("./Routes/plantRoutes");
+const connectDB = require("./database/db");
 require("dotenv").config();
 
 const app = expres();
 app.use(expres.json());
 app.use(cors());
 
-const mongodb_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT;
 
 app.use("/web/api/plants", router);
 
-mongoose
-  .connect(mongodb_URI)
-  .then(() => console.log("connected successfully"))
-  .catch((error) => console.log(error.message));
+connectDB();
+
 app.listen(PORT, () => console.log("Server running on port 3000"));
