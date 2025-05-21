b45e7ab0bdf434ad8fec24d03248d69b687659d7
Layek Miah <layekofficial63@gmail.com>
2025-05-21 12:24:44 +0600

    setup Schema

---
+++ models/plant.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const plantSchema = new mongoose.Schema(
+  {
+    image: { type: String, required: true },
+    name: { type: String, required: true },
+    category: { type: String, required: true },
+    description: { type: String, required: true },
+    careLevel: { type: String, required: true },
+    wateringFrequency: { type: String, required: true },
+    lastWateredDate: { type: Date, required: true },
+    nextWateringDate: { type: Date, required: true },
+    healthStatus: { type: String, required: true },
+    userEmail: { type: String, required: true },
+    userName: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const plantsModel = mongoose.model("plants", plantSchema);
+module.exports = plantsModel;
--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-const expres = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const expres = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = expres();
 
-app.use(expres.json())
+app.use(expres.json());
 app.use(cors());
 
 const mongodb_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.send('hello')
+})
+
+mongoose
+  .connect(mongodb_URI)
+  .then(() => console.log("connected successfully"))
+  .catch((error) => console.log(error.message));
+
+app.listen(PORT, () => console.log("Server running on port 3000"));
